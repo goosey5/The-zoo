4d9c081b2a72267c7c6fcff3c1f545d3029ed23e
goosey5 <165173856+goosey5@users.noreply.github.com>
2024-04-22 22:11:06 +0300

    Add files via upload

--- The zoo/src/Main.java
+++ The zoo/src/Main.java
@@ -8,7 +8,7 @@ public class Main {
         for (int i = 0; i < amountOfAnimals; i++) {
             animalsList.add(createAnimal());
         }
-        printer();
+        animalPrinter();
 
         int moreActions = nextAction();
         Scanner asking = new Scanner(System.in);
@@ -17,7 +17,7 @@ public class Main {
         while (isWhile == 1) {
             if (moreActions == 1) {
                 animalsList.add(createAnimal());
-                printer();
+                animalPrinter();
 
                 System.out.println("Do you want to make more actions? if yes enter 1," +
                         " if you don't want more actions press 0");
@@ -34,17 +34,40 @@ public class Main {
                     if (animalsList.get(i).name.equals(getName) || animalsList.get(i).nickName.equals(getName)) {
                         animalsList.remove(i);
                         System.out.println(getName + " has been removed.");
-                        printer();
+                        animalPrinter();
+
+                        System.out.println("Do you want to make more actions? if yes enter 1," +
+                                " if you don't want more actions press 0");
+                        isWhile=asking.nextInt();
+                        if (isWhile==1)
+                        {
+                            moreActions=nextAction();
+                        }
+                    }
+                    else
+                    {
+                        getName=errorInGettingAnimalsIdentityToDelete();
                     }
                 }
             }
             //stop da program
             else if (moreActions == 3) {
-                isWhile = 0;
+                Scanner checkAgain = new Scanner(System.in);
+                System.out.println("Are you sure you want to stop the program?");
+                String reAssuring=checkAgain.nextLine();
+                if(reAssuring.equalsIgnoreCase("yes")||reAssuring.equalsIgnoreCase("im sure")){
+                    isWhile = 0;
+                }
+                else
+                {
+                    System.out.println("I knew it!");
+                    moreActions=nextAction();
+                }
             }
+
             else
             {
-                nextActionTwo();
+                moreActions=nextActionTwo();
             }
         }
     }
@@ -53,6 +76,26 @@ public class Main {
         Scanner animals = new Scanner(System.in);
         System.out.println("How many animals would you like to add to the zoo?");
         int amountOfAnimals=animals.nextInt();
+        int doesWhile=1;
+
+        while(doesWhile==1){
+            if(amountOfAnimals>10)
+            {
+                System.out.println("You cant add more than 10 animals to the zoo.(it'll be too big!)");
+                System.out.println("please choose another amount of animals that you would like to add to the zoo.");
+                amountOfAnimals=animals.nextInt();
+            }
+            else if (amountOfAnimals<0)
+            {
+                System.out.println("You cant create negative amount of animals, choose another number.");
+                amountOfAnimals=animals.nextInt();
+            }
+            else
+            {
+                doesWhile=0;
+            }
+        }
+
         return amountOfAnimals;
     }
 
@@ -70,17 +113,30 @@ public class Main {
         a.name=information.nextLine();
         System.out.println("What is the nick-name of the animal?");
         a.nickName=information.nextLine();
+        int doesWhile=1;
+
+        while(doesWhile==1)
+        if(a.name.equals(a.nickName))
+        {
+            System.out.println("The nick-name of an animal cant be the same as its name. choose another nick-name.");
+            a.nickName=information.nextLine();
+        }
+        else
+        {
+            doesWhile=0;
+        }
+
         return a;
     }
     //prints the name+nickname of the animal
-    static void printer()
+    static void animalPrinter()
     {
         for(int i=0;i<animalsList.size();i++)
         {
             animalsList.get(i).print();
         }
     }
-    //
+
     static int nextAction()
     {
         Scanner nextAction = new Scanner(System.in);
@@ -97,17 +153,18 @@ public class Main {
         int check=nextAction.nextInt();
         return check;
     }
-    static int addAnimal()
-    {
-        Scanner adder = new Scanner(System.in);
-        System.out.println("Which animal would you like to add to the zoo?");
-        int amountOfAnimals=adder.nextInt();
-        return amountOfAnimals;
-    }
     static String getNameOfAnimalToRemove()
     {
         Scanner deleter = new Scanner(System.in);
         System.out.println("State the name or nickname of the animal that you would like to remove from the zoo:");
+        String animalToDelete=deleter.nextLine();
+        return animalToDelete;
+    }
+    static String errorInGettingAnimalsIdentityToDelete()
+    {
+        Scanner deleter = new Scanner(System.in);
+        System.out.println("The name or nickname that you wrote is not in the list," +
+                "please state the name or nickname of the animal that you would like to remove from the zoo again:");
         String animalToDelete=deleter.nextLine();
         return animalToDelete;
     }
